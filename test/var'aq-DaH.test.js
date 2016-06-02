a4b9b01c6fda8de824d9c4d99f62df35ad521626
Ben Tucker <bjtucker_varaq@youlose.org>
2016-06-02 02:08:10 -0500

    removed js tests

--- test/var'aq-DaH.test.js
+++
@@ -1,8 +0,0 @@
-QUnit.module('General');
-
-QUnit.test('some basic tests', 2, function (assert) {
-	assert.equal('Foo', 'Foo', 'Similar strings are.. equal');
-	// example of failing test
-	assert.equal('This string is not the same', 'as this one', 'different strings are.. different, this fails!');
-
-});
